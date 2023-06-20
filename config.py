d874535d17c0646826a0226c03881477ca273ede
ghwmx <113017673+ghwmx@users.noreply.github.com>
2023-06-20 22:29:06 +0800

    Update config.py

--- config.py
+++ config.py
@@ -20,3 +20,12 @@ birthday = "2001-1-1"
 # 在一起的日子，格式同上
 love_date = "2001-1-1"
 
+# 模板 1：每日提醒模板
+# 本周是开学的第: {{weeks.DATA}} 周
+# 今天是: {{date.DATA}}
+# 城市： {{city.DATA}}
+# 天气： {{weather.DATA}}
+# 最低气温: {{min_temperature.DATA}}
+# 最高气温: {{max_temperature.DATA}}
+# 今天是破壳日的第: {{love_day.DATA}} 天
+# 距离开学还有: {{birthday.DATA}} 天
